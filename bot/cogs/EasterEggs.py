8b0f1b9b42df28414a08987d0a6a051bd5da7d24
Johannes Snildal <johannes.snildal@gmail.com>
2024-08-22 22:03:33 +0200

    improvements to easter eggs cog

--- bot/cogs/EasterEggs.py
+++ bot/cogs/EasterEggs.py
@@ -14,38 +14,35 @@ class EasterEggs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot:
-            return
-
-        if "hello there" in message.content.lower():
-            await message.channel.send("General Kenobi!")
-        elif "i am your father" in message.content.lower():
-            await message.channel.send("Noooooo!")
-    # Responds to messages containing some references to Star Wars. ^
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
         if message.author == self.bot.user:
             return
         
-        rude_words = ["stupid", "idiot", "dumb"]
+        rude_words = ["stupid", "idiot", "dumb", "ugly", "loser"]
         if any(word in message.content.lower() for word in rude_words):
-            insults = [
-                "Ye scallywag!",
-                "Ye barnacle-covered buffoon!",
-                "Ye lily-livered landlubber!",
-                "Ye scurvy pirate!",
-                "Ye yellow-bellied sea dog!",
-                "Ye bilge rat!",
-                "Ye hornswaggler!",
-                "Ye swindler!",
-                "Ye scoundrel!",
-                "Ye scallywag!",
-                "Ye scurvy dog!"
-                "Ye dusty barrel of rum!"
-            ]
-            await message.channel.send(random.choice(insults))
-    # Responds to messages containing rude words with pirate insults ^
+            if "parry" in message.content.lower():
+                insults = [
+                    "scallywag!",
+                    "barnacle-covered buffoon!",
+                    "lily-livered landlubber!",
+                    "scurvy pirate!",
+                    "yellow-bellied sea dog!",
+                    "bilge rat!",
+                    "hornswaggler!",
+                    "swindler!",
+                    "scoundrel!",
+                    "scallywag!",
+                    "scurvy dog!",
+                    "dusty barrel of rum!"
+                ]
+                await message.channel.send("Shut up ya " + random.choice(insults))
+    # Responds to messages containing rude words with responses as pirate insults ^
+
+        if "hello there" in message.content.lower():
+            await message.channel.send("General Kenobi!")
+        
+        elif "i am your father" in message.content.lower():
+            await message.channel.send("Noooooo!")
+    # Responds to messages containing some references to Star Wars. ^
 
 def setup(bot):
     bot.add_cog(EasterEggs(bot))
